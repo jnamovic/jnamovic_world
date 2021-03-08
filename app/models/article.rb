626d66cb313597eb43e35a2d37ca1cc9c8181edb
aiyana <xinyih@andrew.cmu.edu>
2021-03-08 14:00:21 -0800

    Added Article Model

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+    belongs_to: category
+
+    scope :alphabetical, -> { order('title') }
+    scope :active, 	-> { where('active = ?', true) }
+
+    validates_presence_of :title, :content
+end
